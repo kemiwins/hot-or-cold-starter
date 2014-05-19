2fe533a59c02232042849b814be58c26fe853b51
kemiwins <kemi.mail@gmail.com>
2014-05-19 19:13:40 -0400

    fixed number generator to reference magic
    Previously listed var magic in newGame() which created a new variable
    with same name instead of referencing existing number generator.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 
 $(document).ready(function(){
-	
+	 
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -13,31 +13,20 @@ $(document).ready(function(){
 
   	/*--- My code ---*/
 
-
-    $("#guessButton").on("click",function(){
+    $("#guessButton").on("click", function(event){
       guessing();
-      return false;
+      event.preventDefault();
     });
 
-    $(".new").on("click",function(){
+    $(".new").on("click", function(){     
       newGame();
-      var secretNum = function(random){
-      }
-      var magic = Math.floor((Math.random()*100) + 1); 
-      secretNum(magic);
-      alert(magic); 
-    });
-  });
+    });  
+});     
+    var magic = Math.floor((Math.random()*100) + 1);
 
-var secretNum = function(random){
-  }
-var magic = Math.floor((Math.random()*100) + 1); 
-secretNum(magic);
-alert(magic); 
-
-var guessing = function(){    
-    var guess = $("#userGuess ").val();
-    var proximity = (Math.abs(magic-guess));  
+    var guessing = function(){    
+      var guess = $("#userGuess ").val();
+      var proximity = (Math.abs(magic-guess));  
      
       if(guess >= 101 || guess <=0 || guess%1 != 0 || isNaN(guess)){
         alert("Guess a whole number between 1 and 100!");
@@ -45,23 +34,18 @@ var guessing = function(){
         return false;
       }
       if((proximity<=100) && (proximity>=50) ) {
-        alert("FREEZING");
         $("#feedback").text("FREEZING!");
       }
       if((proximity<=49) && (proximity>=30) ) {
-        alert("COLD");
         $("#feedback").text("COLD");
       }
       if((proximity<=29) && (proximity>=10) ) {
-        alert("WARM");
         $("#feedback").text("WARM :)");
       }
       if((proximity<=9) && (proximity>=1) ) {
-        alert("HOT");
         $("#feedback").text("HOT HOT HOT!");
       }
-      if(proximity<=0) {
-        alert("YOU WIN");           
+      if(proximity<=0) {           
         $("#feedback").text("YOU WIN!");
         $("#userGuess").prop("disabled",true );
       }
@@ -72,11 +56,12 @@ var guessing = function(){
       var countGuesses = $("#guessList").children().length;
       $("#count").text(countGuesses);
        return false;
-     }
+     };
 
-var newGame = function(){
+    var newGame = function(){
       $("#feedback").text("Make your Guess!");
       $("#userGuess").prop("disabled",false);
       $("#guessList").text("");
-      $("#count").text("0");
-}
+      $("#count").text("0");      
+      magic = Math.floor((Math.random()*100) + 1);
+     };   
